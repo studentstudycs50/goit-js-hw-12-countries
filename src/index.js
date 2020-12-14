2bfe62ddd40c39b3beffee9bdcb2907f396a525e
Svetlana Veklych <veklychsveta@gmail.com>
2020-12-14 11:26:21 +0200

    first

--- src/index.js
+++ src/index.js
@@ -1,30 +1,55 @@
-//import "./css/styles.css"
-//import template from "./template/template.hbs"
-const { alert, error, notice, defaultModules} = require('@pnotify/core');
+import "./css/styles.css";
+import debounce from 'lodash.debounce';
+import template from '../src/template/template.hbs';
+import templates from '../src/template/template-s.hbs';
+import fetchCountries from '../src/js/fetchCountries';
+const { error, defaultModules } = require('@pnotify/core');
 import '../node_modules/@pnotify/core/dist/PNotify.css'
-import '../node_modules/@pnotify/core/dist/Angeler.css'
 import '../node_modules/@pnotify/core/dist/BrightTheme.css'
-import '../node_modules/@pnotify/core/dist/Material.css'
 
-//const {alert, error, notice, defaultModules} = require('@pnotify/core')
-const myAlert = alert({
-    title: 'Attention',
-    text: "i'm no alert",
-    type: 'info'
-})
-console.log(myAlert);
+const list = document.querySelector('.list');
+const input = document.querySelector('.input');
 
-//const { alert, error, notice,  defaultModules } = require('@pnotify/core')
-const myError = error({
-    title: 'Error',
-    text: "i'm an error",
-    type: 'info'
-})
-console.log(myError);
+function findCountries(event) {
+  const inputValue = event.target.value;
+  if (inputValue) {
+    fetchCountries(inputValue).then(country => {
+      if (country.length === 1) {
+        list.innerHTML = template(country[0]);
+        return;
+      } else if (country.length > 1 && country.length < 11) {
+        list.innerHTML = templates(country);
+        return;
+      } else if (country.length > 11) {
+    error({
+        text: "Too many matches found. Please enter a more specific query!",
+        type: 'info',
+        delay: 1500,
+      })
+      }
+    });
+  }
+  list.innerHTML = '';
+}
 
-const myNotice = notice({
-    title: 'Notice',
-    text: "i'm notice",
-    type: 'info'
-})
-console.log(myNotice);
+input.addEventListener('input', debounce(findCountries, 500));
+
+// import '../node_modules/@pnotify/core/dist/Angeler.css'
+// import '../node_modules/@pnotify/core/dist/Material.css'
+
+// const myAlert = alert({
+//     title: 'Attention',
+//     text: "i'm no alert",
+//     type: 'info'
+// })
+// console.log(myAlert);
+
+
+// const myNotice = notice({
+//   text: "The string is empty, enter the name of the country!",
+//   type: 'info',
+//   delay: 1500,
+// })
+//console.log(myNotice);
+
+
